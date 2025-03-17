addd9087e3d1d5ecad4e4b4e4bcbc5a65f35dbf4
Enzo Martinez <119120079+EnzoMartinez01@users.noreply.github.com>
2025-03-16 23:02:46 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require '../src/FormsController.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -15,6 +20,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $formController = new FormsController();
     $response = $formController->saveForms($data);
+
+    header('Content-Type: application/json');
     echo json_encode($response);
 }
 ?>
